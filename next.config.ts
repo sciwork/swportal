82d17ad0e184beb8032b7477e477921d0a665130
5x <longhorn482653@gmail.com>
2025-02-22 14:44:55 +0800

    feat: use remark-gfm

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,24 @@
 import type { NextConfig } from "next";
 import remarkFrontmatter from "remark-frontmatter";
+import remarkGfm from "remark-gfm";
 import createMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   // Optionally, add any other Next.js config below
+  output: "export",
+  distDir: process.env.OUTPUTDIR,
+  images: {
+    unoptimized: true,
+  },
 };
 
 const withMDX = createMDX({
   // Add markdown plugins here, as desired
   options: {
-    remarkPlugins: [remarkFrontmatter],
+    remarkPlugins: [remarkFrontmatter, remarkGfm],
+    rehypePlugins: [],
   },
 });
 
