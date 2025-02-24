0b7fb066dd344fdccfc6625b25178a9fe7e21ce8
5x <longhorn482653@gmail.com>
2025-02-24 23:43:52 +0800

    fix(components/NavLink.tsx): active at subpage

--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -2,6 +2,14 @@ import clsx from "clsx";
 import NextLink from "next/link";
 import { usePathname } from "next/navigation";
 
+const isActive = (href: string, pathname: string) => {
+  if (href === "/") {
+    return pathname === "/";
+  }
+
+  return pathname.startsWith(href);
+};
+
 const NavLink = ({
   className,
   children,
@@ -12,13 +20,15 @@ const NavLink = ({
   href: string;
 }) => {
   const pathname = usePathname();
+  const active = isActive(href, pathname);
+
   return (
     <NextLink
       className={clsx(
         "mx-8 py-2 font-yk text-3xl no-underline hover:text-red-600 lg:mr-4 lg:py-0 lg:text-2xl",
         className,
-        { "active:text-red-600": pathname === href },
-        { "text-white": pathname !== href },
+        { "active:text-red-600": active },
+        { "text-white": !active },
       )}
       href={href}
     >
