99d8dd8f6cbf3e7d9f1b12d984dda8cc54e5e95f
5x <longhorn482653@gmail.com>
2025-03-22 14:24:05 +0800

    feat(mdx-components.tsx): add Map into mdx-components
    Developer can use Map compoment directly in mdx without import

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,7 +1,9 @@
 import type { MDXComponents } from "mdx/types";
+import Map from "@/components/Map";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    Map,
     ...components,
   };
 }
