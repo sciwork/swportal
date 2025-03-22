22c1faeb0a67c7d37e3cf338b6fd07f1f06ba757
ntut-xuan <t109590031@ntut.org.tw>
2025-03-22 16:40:20 +0800

    Adjust: Style

--- components/Meetup.tsx
+++ components/Meetup.tsx
@@ -17,30 +17,45 @@ const GetMaxEventCount = (schedules: ScheduleType[]) => {
   return count;
 };
 
+const TermEmptyLine = (contents: string[]) => {
+  const start =
+    contents[0].length != 0
+      ? 0
+      : contents.findIndex((content) => content.length == 0);
+  const end =
+    contents[contents.length - 1].length != 0
+      ? contents.length - 1
+      : contents.findLastIndex((content) => content.length == 0);
+
+  return contents.slice(start, end);
+};
+
 const EventCard = (props: { event: EventType }) => {
   const [collapsible, setCollapsible] = useState<boolean>(false);
   const Detail = () => {
     return (
       <div className="flex flex-col">
-        {props.event.description.split("\n").map((content, index) => {
-          return (
-            <p className="m-0 min-h-6" key={`descripton-${index}`}>
-              {content}
-            </p>
-          );
-        })}
+        {TermEmptyLine(props.event.description.split("\n")).map(
+          (content, index) => {
+            return (
+              <p className="m-0 min-h-6" key={`descripton-${index}`}>
+                {content}
+              </p>
+            );
+          },
+        )}
       </div>
     );
   };
 
   return (
     <div
-      className="w-full text-sm"
+      className="w-full text-sm md:text-base"
       onClick={() => setCollapsible(!collapsible)}
     >
-      <p className="font-bold">{props.event.title}</p>
+      <p className="text-nowrap font-bold">{props.event.title}</p>
       {props.event.description ? (
-        <div className={`overflow-hidden p-3 text-left`}>
+        <div className={`overflow-hidden text-left`}>
           <Detail />
         </div>
       ) : null}
@@ -70,7 +85,7 @@ const HorizontalTable = (props: { schedules: ScheduleType[] }) => {
                         : 1
                     }
                     key={`${schedule.time}-event-${index}`}
-                    className="rounded-xl border border-[#cccccc]"
+                    className="rounded-xl border border-[#cccccc] p-5"
                   >
                     <EventCard event={event} />
                   </td>
@@ -88,7 +103,7 @@ const VerticalTable = (props: { schedules: ScheduleType[] }) => {
   return (
     <table className="mb-5 w-full border-0">
       <tbody>
-        {props.schedules?.map((schedule, index) => {
+        {props.schedules?.map((schedule) => {
           return (
             <tr key={`${schedule.time}`}>
               <td className="align-middle">
