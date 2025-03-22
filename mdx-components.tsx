1873741999d9847e981e124bbbb2908624b9dea3
5x <longhorn482653@gmail.com>
2025-03-22 22:17:32 +0800

    refactor: remove unused file

--- mdx-components.tsx
+++
@@ -1,9 +0,0 @@
-import type { MDXComponents } from "mdx/types";
-import Map from "@/components/Map";
-
-export function useMDXComponents(components: MDXComponents): MDXComponents {
-  return {
-    Map,
-    ...components,
-  };
-}
