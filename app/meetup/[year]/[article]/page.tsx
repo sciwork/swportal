e055d8cb591c2cca2fd8e3789502500a9cbf0f23
5x <longhorn482653@gmail.com>
2025-03-23 15:31:49 +0800

    refactor: remove unnecessary function call

--- app/meetup/[year]/[article]/page.tsx
+++ app/meetup/[year]/[article]/page.tsx
@@ -5,8 +5,6 @@ const collection = "meetup";
 const { generateStaticParams, generateMetadata, Page } =
   buildCollection(collection);
 
-generateStaticParams();
-
 export { generateStaticParams, generateMetadata };
 
 export default Page;
--- app/training/[year]/[article]/page.tsx
+++ app/training/[year]/[article]/page.tsx
@@ -5,8 +5,6 @@ const collection = "training";
 const { generateStaticParams, generateMetadata, Page } =
   buildCollection(collection);
 
-generateStaticParams();
-
 export { generateStaticParams, generateMetadata };
 
 export default Page;
