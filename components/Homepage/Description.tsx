a3c87c5ef91f75b588fa03dfffe924ab85560793
rockleona <34214497+rockleona@users.noreply.github.com>
2025-04-20 22:48:42 +0800

    fix(homepage/description): fix negative behavior that linter detected

--- components/Homepage/Description.tsx
+++ components/Homepage/Description.tsx
@@ -8,9 +8,6 @@ import ScrollTrigger from 'gsap/ScrollTrigger';
 
 import "./Hero.css"; // Import your CSS file for styles
 
-import Image from "next/image";
-import iconImg from "@/assets/android-chrome-512x512.png";
-
 gsap.registerPlugin(useGSAP, MotionPathPlugin, ScrollTrigger); // register the MotionPathPlugin
 
 // ------- Kudos to Osmo [https://osmo.supply/] ------- //
@@ -39,7 +36,7 @@ const HomepageDescription: React.FC = () => {
         <div ref={container} className="relative flex items-center justify-center w-full h-screen bg-cover bg-center bg-no-repeat bg-black opacity-70">
             <section className="cloneable panel orange">
                 <span className="line line-2"></span>
-                <p>This orange panel gets pinned when its top edge hits the top of the viewport, then the line's animation is linked with the scroll position until it has traveled 100% of the viewport's height (<code>end: "+=100%"</code>), then the orange panel is unpinned and normal scrolling resumes. Padding is added automatically to push the rest of the content down so that it catches up with the scroll when it unpins. You can set <code>pinSpacing: false</code> to prevent that if you prefer.</p>
+                <p>This orange panel gets pinned when its top edge hits the top of the viewport, then the line animation is linked with the scroll position until it has traveled 100% of the viewport height, then the orange panel is unpinned and normal scrolling resumes. Padding is added automatically to push the rest of the content down so that it catches up with the scroll when it unpins. You can set <code>pinSpacing: false</code> to prevent that if you prefer.</p>
             </section>
         </div>
     );
