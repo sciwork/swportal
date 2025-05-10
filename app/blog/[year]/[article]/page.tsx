5d5a381df50b14d8a76608f6e431e20397571aac
Chris <j8xixomelkor@icloud.com>
2025-05-10 22:44:42 +0800

    feat: add date & author in blog page

--- app/blog/[year]/[article]/page.tsx
+++ app/blog/[year]/[article]/page.tsx
@@ -1,12 +1,37 @@
-import { buildCollection } from "@/utils/collection";
+import { buildCollection, Collection } from "@/utils/collection";
+import dayjs from "dayjs";
 
 const collection = "blog";
 
 const { generateStaticParams, generateMetadata, Page } =
   buildCollection(collection);
 
+const BlogPage = async({params,}:{params :Promise<{year:string ; article:string}>}) =>{
+  const { year, article } = await params;
+  const collectionData = new Collection("blog");
+  const data = await collectionData.getArticleData(year, article);
+  
+  return (
+    <div className="max-w-4xl mx-auto px-4 pb-8">
+      <div className="mb-8">
+        <footer className="post-info text-gray-600">
+          <time className="published" dateTime={dayjs(data.date).format("YYYY-MM-DDTHH:mm:ssZ")}>
+            {dayjs(data.date).format("ddd DD MMM YYYY")}
+          </time>
+          {data.author && (
+            <address className="vcard author text-gray-600">
+              By {data.author}
+            </address>
+          )}
+        </footer>
+      </div>
+      <Page params={params} />
+    </div>
+  );
+};
+
 generateStaticParams();
 
 export { generateStaticParams, generateMetadata };
 
-export default Page;
+export default BlogPage;
