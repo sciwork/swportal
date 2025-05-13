366ecd6002f1ef7839318e1b1fac7af57580732c
5x <longhorn482653@gmail.com>
2025-05-13 21:21:04 +0800

    feat(app/meetup/page.tsx): fetch meetup list from collections

--- app/meetup/page.tsx
+++ app/meetup/page.tsx
@@ -1,12 +1,38 @@
 import { Metadata } from "next";
-import Content from "@/components/Content";
+import NextLink from "next/link";
+import { ArticleType, Collection } from "@/utils/collection";
 
 export const metadata: Metadata = {
   title: "Meetup",
 };
 
-const Page: React.FC = () => {
-  return <Content filePath="meetup/index.mdx" />;
+const Page: React.FC = async () => {
+  const collection = new Collection("meetup");
+  const meetups = await collection.listByYear();
+
+  return (
+    <section>
+      {Array.from(meetups.keys()).map((year) => {
+        const articles = meetups.get(year) || [];
+        return (
+          <div key={year} className="mb-10">
+            <h3>Meetup {year}</h3>
+            <ul>
+              {articles.map((article: ArticleType) => (
+                <li key={article.article.title}>
+                  <NextLink
+                    href={`/meetup/${article.params.year}/${article.params.article}`}
+                  >
+                    {article.article.title}
+                  </NextLink>
+                </li>
+              ))}
+            </ul>
+          </div>
+        );
+      })}
+    </section>
+  );
 };
 
 export default Page;
