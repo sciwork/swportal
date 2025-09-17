45a25e18970c6adee3de216c8d1628bbb9e3ce0e
ntut-xuan <t109590031@ntut.org.tw>
2025-09-17 21:21:24 +0800

    Remove redundant div

--- components/ArticleCard.tsx
+++ components/ArticleCard.tsx
@@ -13,9 +13,7 @@ export const ArticleCard: React.FC<ArticleCardProps> = ({ article, hideDate = fa
     <div className="border rounded w-100 p-5 mb-3 hover:shadow-lg duration-200 transition-all cursor-pointer flex flex-row gap-3 border-red-800">
       <FontAwesomeIcon icon={faCalendar} className="my-auto text-red-800" size="2xl"></FontAwesomeIcon>
       <div className="flex flex-col">
-        <div>
-          <h5 className="m-0 text-black">{article.article.title}</h5>
-        </div>
+        <h5 className="m-0 text-black">{article.article.title}</h5>
         { article.article.location &&
             <div className="flex flex-row gap-1">
             <FontAwesomeIcon icon={faMapMarker} className="text-gray-500 w-5 my-auto"></FontAwesomeIcon>
